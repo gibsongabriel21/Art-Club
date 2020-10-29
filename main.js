8ddc259b8a176349229183aa4cf34a4f9dd9cfc4
Gabriel Gibson <69363005+gibsongabriel21@users.noreply.github.com>
2020-10-29 13:11:57 -0500

    slideshow edit

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-var slideIndex = 0;
-showSlides();
+$("#slideshow > div:gt(0)").hide();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex + 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+setInterval(function () {
+  $("#slideshow > div:first")
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo("#slideshow");
+}, 3000);
